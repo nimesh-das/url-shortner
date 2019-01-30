04fe03b4b77172712b33654ee047dfe8c77e1148
Nimesh Das <ndas@opentext.com>
2019-01-30 05:59:45 -0500

    haven't compiled this version yet

--- url-shortner.py
+++ url-shortner.py
@@ -3,15 +3,13 @@ import hashlib
 import base64
 import string
 import random
+import sqlite3
 
-#hash_object = hashlib.md5(b'Hello World')
-#print(hash_object.hexdigest())
+# External database for strategy # 2
+if (os.path.isfile("database.db") == False):
+	db = sqlite3.connect('database.db')
+	db.execute('create table shortner (url_code text, url_full text)')
 
-#data = base64.b64encode(b'data to be encoded')
-#print(data)
-
-# External dictionary/database
-new_dict = dict()
 
 class Shortner:
 
@@ -19,11 +17,16 @@ class Shortner:
 		return ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
 		
 	def add_url(self):
-		#either check in database if url exists
-		# or if you don't have database, check in the dictionary
+		#check in the database whether url exists
 		new_code = self.code_generator()
-		while new_code in new_dict:
-			new_code = code_generator()
+		cur = db.cursor()
+		cur.execute("select * from shortner where url_code=?", new_code)
+		data = cur.fetchall()
+		while (data is not None):
+			new_code = self.code_generator()
+			cur.execute("select * from shortner where url_code=?", new_code)
+			data = cur.fetchall()
+		
 		#when a unique code has been found, enter it in the dictionary
 		result = "http://bit.ly/" + new_code;
 		url = input("Please enter your url: ")
